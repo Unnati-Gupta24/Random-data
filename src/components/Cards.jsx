8bd6b30b4db54dcc70f81fd4369cd56f2e49136e
Unnati gupta <guptaunnati031@gmail.com>
2024-07-13 18:19:51 +0530

    Add files via upload

---
+++ src/components/Cards.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Cards.css"
+
+const Cards = () => {
+  return (
+    <div class="box">
+    <div className='card'>
+      <h1>This is the heading</h1>
+      <p>The main content of card</p>
+    </div>
+    <div className='card'>
+      <h1>This is the heading</h1>
+      <p>The main content of card</p>
+    </div>
+    <div className='card'>
+      <h1>This is the heading</h1>
+      <p>The main content of card</p>
+    </div>
+ </div>
+  )
+}
+
+export default Cards
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      copyright @Stunning_whiz24||All rights reserved
+    </div>
+  )
+}
+
+export default Footer
